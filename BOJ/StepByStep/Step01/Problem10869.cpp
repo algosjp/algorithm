ea67b16e4f7e1d5154f479ed36357c3bfb652ce6
algosjp <algosjp@gmail.com>
2025-07-22 14:28:12 +0900

    Step01

---
+++ BOJ/StepByStep/Step01/Problem10869.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << a+b << "\n";
+    cout << a-b << "\n";
+    cout << a*b << "\n";
+    cout << a/b << "\n";
+    cout << a%b << "\n";
+
+    return 0;
+}
---
+++ BOJ/StepByStep/Step01/Problem2588.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    int digits[4];
+    digits[3] = num1*num2;
+    for (int i=0; i<3; i++) {
+        digits[i] = num2%10;
+        num2 /= 10;
+    }
+
+    for (int i=0; i<3; i++) {
+        cout << num1*digits[i] << "\n";
+    }
+    cout << digits[3] << "\n";
+
+    return 0;
+}
