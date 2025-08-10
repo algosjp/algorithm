8eb97d0d7d40347f6ff3e01c65b6b25366e5dad5
algosjp <algosjp@gmail.com>
2025-08-10 17:11:17 +0900

    Step16

---
+++ BOJ/StepByStep/Step16/Problem12789.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> waitline;
+    int target = 1, ticket;
+    while (true) {
+        while(!waitline.empty() && waitline.back() == target) {
+            waitline.pop_back();
+            target += 1;
+        }
+
+        if (target > N) {
+            break;
+        }
+
+        cin >> ticket;
+
+        if (ticket == target) {
+            target += 1;
+        }
+        else {
+            if (!waitline.empty() && waitline.back() < ticket) {
+                cout << "Sad" << "\n";
+                return 0;
+            }
+
+            waitline.push_back(ticket);
+        }
+    }
+
+    cout << "Nice" << "\n";
+
+    return 0;
+}
